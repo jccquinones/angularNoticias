57a2c92653e16fa17460c5d96f290810799b37d0
John <jqm004@alumnos.ucn.cl>
2018-05-15 23:32:21 -0400

    Updated: Articles services

--- src/app/service/articles.service.ts
+++ src/app/service/articles.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { HttpClientModule, HttpHeaders, HttpRequest, HttpResponse} from '@angular/common/http';
+import 'rxjs/add/operator/map';
+import { Observable} from "rxjs/Observable";
+import { Article} from "../model/article";
+import { GlobalService} from "./global.service";
 
 @Injectable()
 export class ArticlesService {
 
-  constructor() { }
+  public url: string;
+
+  constructor(
+    public _http: Http,
+    public _url: GlobalService
+  ) {
+    this.url = _url.getUrl();
+  }
+
+  getArticles(){
+
+    return this._http.get(this.url).map(res => res.json());
+  }
 
 }
