cd1b3f3dc1581ab2cefae9ff90a50e4fed3f3195
John <jqm004@alumnos.ucn.cl>
2018-05-15 23:40:26 -0400

    Updated: Articles list component

--- src/app/component/articles-list/articles-list.component.ts
+++ src/app/component/articles-list/articles-list.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params} from "@angular/router";
+import { ArticlesService} from "../../service/articles.service";
+import { Article} from "../../model/article";
 
 @Component({
   selector: 'app-articles-list',
   templateUrl: './articles-list.component.html',
-  styleUrls: ['./articles-list.component.css']
+  styleUrls: ['./articles-list.component.css'],
+  providers: [ArticlesService]
 })
 export class ArticlesListComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+}
+
+export class ArticlesListComponent implements OnInit {
+
+  public articles: Article[];
+
+  constructor(
+    private articleService: ArticlesService,
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) { }
+
+  ngOnInit() {
+
+    this.articleService.getArticles().subscribe(
+
+      result => {
+        // se retornan los articulos de api-news
+        this.articles = result.articles;
+        console.log(result.articles);
+      }
+    )
+
   }
 
 }
