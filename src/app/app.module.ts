a3359cb275c013794ece3c8b78fcdc06c680bff3
John <jqm004@alumnos.ucn.cl>
2018-05-15 22:58:08 -0400

    Added: Importacion modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatGridListModule} from '@angular/material/grid-list';
+import 'hammerjs';
+
+import {
+  MatListModule,
+  MatInputModule,
+  MatChipsModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatButtonModule,
+  MatCardModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatTabsModule,
+  MatIconModule
+} from '@angular/material';
+
 
 
 @NgModule({
@@ -10,9 +31,26 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserAnimationsModule,
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatCardModule,
+    MatChipsModule,
+    MatIconModule,
+    MatInputModule,
+    MatGridListModule,
+    MatListModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatTabsModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
