448b9966ad2942bf5343bd48bfcb3ef849185ad8
John <jqm004@alumnos.ucn.cl>
2018-05-15 23:09:50 -0400

    Added: opciones on home component

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -5,7 +5,15 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
 export class HomeComponent implements OnInit {
+
+  color: string;
+  opciones = [
+    { name: 'Primary', color: 'primary'},
+    { name: 'Accent', color: 'accent'},
+    { name: 'Warn', color: 'warn'}
+  ];
 
   constructor() { }
 
