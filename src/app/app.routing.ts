6acb2c3ef14335c1ec22f1070438547a76c18958
Jose Diaz <josediazv04@gmail.com>
2018-05-17 20:50:26 -0400

    Updated: app.routing.ts, ruta de detalles de favoritos añadida.

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,6 +7,7 @@ import {ErrorComponent} from './component/error/error.component';
 import {ArticlesListComponent} from './component/articles-list/articles-list.component';
 import {ArticleDetailComponent} from './component/article-detail/article-detail.component';
 import {FavoritesListComponent} from './component/favorites-list/favorites-list.component';
+import {FavoriteDetailComponent} from './component/favorite-detail/favorite-detail.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent},
@@ -14,6 +15,7 @@ const appRoutes: Routes = [
   { path: 'listado', component: ArticlesListComponent},
   { path: 'article-detail/:id', component: ArticleDetailComponent},
   { path: 'favorites', component: FavoritesListComponent},
+  { path: 'favorite-detail/:id', component: FavoriteDetailComponent},
   { path: '**', component: ErrorComponent}
 ];
 
